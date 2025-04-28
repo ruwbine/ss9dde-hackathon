85bc8f8d21010b8bd646f2279d9398ad1142306c
aetery- <kurthamachi@mail.ru>
2025-04-28 16:20:17 +0500

    jwt secret fix

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -2,18 +2,21 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { UsersService } from '../../users/users.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
   constructor(
       private readonly usersService: UsersService,
+      private readonly configService: ConfigService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET
+      secretOrKey: configService.get<string>('JWT_SECRET') !
     });
   }
+
 
 
   async validate(payload: any) {
